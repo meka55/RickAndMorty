355eef17b90ce3e5bd03b4bc6bffc614b4553f8c
TrbekovaMeerim <meerimturarbekova@gmail.com>
2022-10-31 16:33:44 +0600

    подключила Repository,okttps

--- app/src/main/java/com/example/rickandmorty/data/RetrofitClient.kt
+++ app/src/main/java/com/example/rickandmorty/data/RetrofitClient.kt
@@ -11,8 +11,19 @@ import java.util.concurrent.TimeUnit
 
 class RetrofitClient {
 
-    var retrofit: Retrofit = Retrofit.Builder()
+    private val okHttpClient: OkHttpClient =
+        OkHttpClient().newBuilder().addInterceptor(providerInspector())
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS).build()
+
+    private fun providerInspector(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    }
+
+    private var retrofit: Retrofit = Retrofit.Builder()
         .baseUrl("https://rickandmortyapi.com/")
+        .client(okHttpClient)
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
@@ -23,6 +34,7 @@ class RetrofitClient {
     fun provideEpisodeApiService(): EpisodeApi {
         return retrofit.create(EpisodeApi::class.java)
     }
+
     fun provideLocationApiService(): LocationApi {
         return retrofit.create(LocationApi::class.java)
     }
