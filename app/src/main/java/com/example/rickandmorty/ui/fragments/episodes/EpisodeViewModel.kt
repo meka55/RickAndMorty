da87481207c2772309753819e63e46cb1ef3c87f
TrbekovaMeerim <meerimturarbekova@gmail.com>
2022-10-31 16:51:21 +0600

    подключила Repository,okttps.

--- app/src/main/java/com/example/rickandmorty/ui/fragments/episodes/EpisodeViewModel.kt
+++ app/src/main/java/com/example/rickandmorty/ui/fragments/episodes/EpisodeViewModel.kt
@@ -3,33 +3,18 @@ package com.example.rickandmorty.ui.fragments.episodes
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.rickandmorty.App
+import com.example.rickandmorty.data.repositories.CharacterRepository
+import com.example.rickandmorty.data.repositories.EpisodeRepository
 import com.example.rickandmorty.models.RickAndMortyResponse
+import com.example.rickandmorty.models.character.CharacterModel
 import com.example.rickandmorty.models.episode.EpisodeModel
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class EpisodeViewModel : ViewModel() {
-
-    val data: MutableLiveData<RickAndMortyResponse<EpisodeModel>> = MutableLiveData()
-
+    private val repository = EpisodeRepository()
     fun fetchEpisode(): MutableLiveData<RickAndMortyResponse<EpisodeModel>> {
-        App.episodeApi?.fetchEpisode()
-            ?.enqueue(object : Callback<RickAndMortyResponse<EpisodeModel>> {
-                override fun onResponse(
-                    call: Call<RickAndMortyResponse<EpisodeModel>>,
-                    response: Response<RickAndMortyResponse<EpisodeModel>>
-                ) {
-                    data.value = response.body()
-                }
-
-                override fun onFailure(
-                    call: Call<RickAndMortyResponse<EpisodeModel>>,
-                    t: Throwable
-                ) {
-                    data.value = null
-                }
-            })
-        return data
+        return repository.fetchEpisode()
     }
 }
--- app/src/main/java/com/example/rickandmorty/ui/fragments/location/LocationViewModel.kt
+++ app/src/main/java/com/example/rickandmorty/ui/fragments/location/LocationViewModel.kt
@@ -3,33 +3,18 @@ package com.example.rickandmorty.ui.fragments.location
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.rickandmorty.App
+import com.example.rickandmorty.data.repositories.EpisodeRepository
+import com.example.rickandmorty.data.repositories.LocationRepository
 import com.example.rickandmorty.models.RickAndMortyResponse
+import com.example.rickandmorty.models.episode.EpisodeModel
 import com.example.rickandmorty.models.location.LocationModel
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class LocationViewModel : ViewModel() {
-
-    val data: MutableLiveData<RickAndMortyResponse<LocationModel>> = MutableLiveData()
-
+    private val repository = LocationRepository()
     fun fetchLocation(): MutableLiveData<RickAndMortyResponse<LocationModel>> {
-        App.locationApi?.fetchLocation()
-            ?.enqueue(object : Callback<RickAndMortyResponse<LocationModel>>{
-                override fun onResponse(
-                    call: Call<RickAndMortyResponse<LocationModel>>,
-                    response: Response<RickAndMortyResponse<LocationModel>>
-                ) {
-                    data.value = response.body()
-                }
-
-                override fun onFailure(
-                    call: Call<RickAndMortyResponse<LocationModel>>,
-                    t: Throwable
-                ) {
-                    data.value = null
-                }
-            })
-        return data
+        return repository.fetchLocation()
     }
 }
